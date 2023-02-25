d529ebdb26ac340f384e5e55f82df0de6284a8f1
vadimvynnchuk <vadimvinnicuk4@gmail.com>
2023-02-25 13:51:53 +0200

    first push

---
+++ app/js/app.js
@@ -0,0 +1,50 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+import Swiper, {
+	Pagination,
+	EffectFade,
+	Autoplay,
+	Navigation,
+	Thumbs
+  } from "swiper";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Swiper.use([Pagination, EffectFade, Autoplay, Navigation,Thumbs]);
+
+	// Custom JS
+
+	var swiperNew = new Swiper(".services-slider", {
+        slidesPerView: 6,
+        spaceBetween: 30,
+
+		breakpoints: {
+            
+            320: {
+              slidesPerView: 2.5,
+              spaceBetween: 20,
+
+            },
+          
+            480: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+    
+            820: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+
+            1000: {
+                slidesPerView: 6,
+            },
+
+
+        }
+
+    });
+
+})
